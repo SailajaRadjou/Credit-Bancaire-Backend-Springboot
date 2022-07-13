c5c927eb43862453dfc8d149dae32d935ae7c42f
SailajaRadjou <p.sailaja@hotmail.com>
2022-07-14 01:06:32 +0200

    created api for updating client details by id

--- src/main/java/com/bancaire/credit/controller/ClientController.java
+++ src/main/java/com/bancaire/credit/controller/ClientController.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -40,6 +41,24 @@ public class ClientController {
 		Client client = clientRepository.findById(id)
 				.orElseThrow(() -> new ResourceNotFoundException("Client not exist with this id : "+id));
 		return ResponseEntity.ok(client);
+	}
+	
+	//update client details
+	@PutMapping("/clients/{id}")
+	public ResponseEntity<Client> updateClient(@PathVariable int id, @RequestBody Client clientUpdate){
+		Client client = clientRepository.findById(id)
+				.orElseThrow(() -> new ResourceNotFoundException("Client not exist with this id : "+id));
+		client.setNom(clientUpdate.getNom());
+		client.setPrenom(clientUpdate.getPrenom());
+		client.setDateNaissance(clientUpdate.getDateNaissance());
+		client.setAdresse(clientUpdate.getAdresse());
+		client.setTel(clientUpdate.getTel());
+		client.setMail(clientUpdate.getMail());
+		client.setCivilite(clientUpdate.getCivilite());
+		
+		Client updatedClient = clientRepository.save(client);
+		return ResponseEntity.ok(updatedClient);
+	
 	}
 
 }

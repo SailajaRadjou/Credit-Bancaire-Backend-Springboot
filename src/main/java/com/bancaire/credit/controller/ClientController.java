60800590468f4f2fbd8ec6e3a7247c8650ca5cc2
SailajaRadjou <p.sailaja@hotmail.com>
2022-07-15 21:57:20 +0200

    modification in the client controller

--- src/main/java/com/bancaire/credit/controller/ClientController.java
+++ src/main/java/com/bancaire/credit/controller/ClientController.java
@@ -1,9 +1,9 @@
 package com.bancaire.credit.controller;
 
 import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,6 +16,8 @@ import com.bancaire.credit.exception.ResourceNotFoundException;
 import com.bancaire.credit.model.Client;
 import com.bancaire.credit.repository.ClientRepository;
 
+
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("/api/")
 public class ClientController {
